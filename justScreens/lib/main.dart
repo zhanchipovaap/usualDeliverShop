723a2e73bdc606d3f11b06d899024bf143f44027
Alex Zhan <jet.ver3@gmail.com>
2024-02-08 16:47:31 +0800

    first splashScreen

--- justScreens/lib/main.dart
+++ justScreens/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:testproject/sceens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,61 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    super.key,
-  });
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    return Scaffold(
-      body: NestedScrollView(
-        body: Container(
-            // decoration: BoxDecoration(
-            //     color: theme.primaryColor,
-            //     image: const DecorationImage(
-            //       image: AssetImage('assets/images/BG.png'),
-            //       fit: BoxFit.cover,
-            //     )),
-            ),
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            SliverAppBar(
-              shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30))),
-              expandedHeight: MediaQuery.of(context).size.height * 0.35,
-              floating: true,
-              pinned: false,
-              snap: true,
-              flexibleSpace: Stack(
-                children: <Widget>[
-                  Container(
-                    decoration: BoxDecoration(
-                        color: theme.primaryColor,
-                        image: const DecorationImage(
-                          image: AssetImage('assets/images/BG2.png'),
-                          fit: BoxFit.cover,
-                        )),
-                  )
-                ],
-              ),
-            )
-          ];
-        },
-      ),
+      home: const SplashScreen(),
     );
   }
 }
--- justScreens/lib/sceens/splash_screen.dart
+++ justScreens/lib/sceens/splash_screen.dart
@@ -1,10 +1,76 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key, this.title});
+
+  final String? title;
 
   @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    final theme = Theme.of(context);
+    return Scaffold(
+      body: NestedScrollView(
+        body: const Column(),
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: MediaQuery.of(context).size.height * 0.35,
+              leading: Container(
+                decoration: BoxDecoration(
+                  color: theme.primaryColor,
+                  image: const DecorationImage(
+                    image: AssetImage('assets/images/LOGO.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: const Column(),
+              ),
+              toolbarHeight: 100,
+              leadingWidth:100,
+              floating: true,
+              pinned: false,
+              snap: true,
+              flexibleSpace: FlexibleSpaceBar(
+                  background: Container(
+                decoration: BoxDecoration(
+                  color: theme.primaryColor,
+                  image: const DecorationImage(
+                    image: AssetImage('assets/images/BG2.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: const Column(),
+              )),
+            ),
+            SliverToBoxAdapter(
+              child: Container(
+                color: theme.primaryColor,
+                height: 20,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      height: 20,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20.0),
+                          topRight: Radius.circular(20.0),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ];
+        },
+      ),
+    );
   }
 }
